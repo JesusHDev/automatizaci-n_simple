262d638570fa63ae02b49da399b99454a60091c3
JesusHDev <jesusvalerio152004@gmail.com>
2026-04-23 18:32:03 -0600

    main.py

--- main.py
+++ main.py
@@ -12,6 +12,14 @@ options = Options()
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--window-size=1920,1080")
+
+# ESTA LÍNEA ES CLAVE: Oculta que es un bot
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+
+options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36")
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
